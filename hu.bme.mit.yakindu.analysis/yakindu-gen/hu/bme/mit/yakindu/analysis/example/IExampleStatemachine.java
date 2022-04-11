6b28f0e0f8fae34dbbbe6de28181eab13e99408b
botondar <m.botond7@gmail.com>
2022-04-11 14:51:59 +0200

    Revert example.sct

--- hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/IExampleStatemachine.java
+++ hu.bme.mit.yakindu.analysis/yakindu-gen/hu/bme/mit/yakindu/analysis/example/IExampleStatemachine.java
@@ -13,8 +13,6 @@ public interface IExampleStatemachine extends ITimerCallback,IStatemachine {
 		
 		public void raiseBlack();
 		
-		public void raiseGreen();
-		
 		public long getWhiteTime();
 		
 		public void setWhiteTime(long value);
